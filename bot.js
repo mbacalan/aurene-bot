1b52df90e2b192f2d062116cac4bff30e450f218
Muhammed Bacalan <muhammedbacalan@gmail.com>
2017-11-21 16:32:20 +0300

    Initial upload

---
+++ bot.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const discord = require("discord.js");
+const {token, owner, prefix} = require("./bot_config.json");
+const commandFiles = fs.readdirSync("./commands");
+
+// Get an instance of Discord Client
+const bot = new discord.Client({
+  commandPrefix: prefix,
+  disableEveryone: true,
+  owner
+});
+bot.commands = new discord.Collection();
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  bot.commands.set(command.name, command);
+}
+
+bot.on("ready", async () => {
+  await console.log(`Client ready; logged in as ${bot.user.username}#${bot.user.discriminator} (${bot.user.id})`);
+  await bot.user.setGame("Guild Wars 2");
+  await console.log(`Bot's presence is set to: ${bot.user.presence.game.name}`);
+});
+
+bot.on("message", message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/\s+/);
+  const commandName = args.shift().toLowerCase();
+
+  if (!bot.commands.has(commandName)) return;
+  const command = bot.commands.get(commandName);
+
+  if (command.args && !args.length) {
+  return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+  }
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("There was an error trying to execute that command!");
+  }
+});
+
+bot.login(token);
+
+// bot.on("message", message => {
+//   if (message.content === `${prf}dungeons`) {
+//     api.dungeons().all().then(result => console.log(result)).catch(err => console.log(err));
+//     }
+// });
+
+// bot.on("message", message => {
+//   if (message.content === `${config.prefix}raids`) {
+//     api.account().raids().get().
+//       then(result => message.reply(result));
+//   }
+// });
+
