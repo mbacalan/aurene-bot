1dc046c01d5882cfeb0bf962a1b6861143ab412e
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-01-30 12:19:08 +0300

    Minor tweaks

--- bot.js
+++ bot.js
@@ -29,10 +29,11 @@ bot.on("ready", async () => {
 bot.on("message", message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).split(/\s+/);
+  const args = message.content.slice(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
 
   if (!bot.commands.has(commandName)) return;
+
   const command = bot.commands.get(commandName);
 
   if (command.args && !args.length) {
@@ -43,7 +44,7 @@ bot.on("message", message => {
     command.execute(message, args);
   } catch (error) {
     console.error(error);
-    message.reply("There was an error trying to execute that command!");
+    message.reply("there was an error trying to execute that command!");
   }
 });
 
