daa6cafa8de0be874ff7e56042f00ad67adf384e
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-01-30 12:55:30 +0300

    Improve arguement check

--- bot.js
+++ bot.js
@@ -37,7 +37,13 @@ bot.on("message", message => {
   const command = bot.commands.get(commandName);
 
   if (command.args && !args.length) {
-    return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
   }
 
   try {
