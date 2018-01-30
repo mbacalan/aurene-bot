b73964f8e26dd37879c2a71bbc6060be1dd967e4
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-01-30 12:21:57 +0300

    Initial guide command

--- commands/guides.js
+++ commands/guides.js
@@ -1,36 +1,20 @@
-const { Command } = require("discord.js-commando");
-
-module.exports = class guidesCommand extends Command {
-  constructor (bot) {
-    super(bot, {
-      name: "guide",
-      group: "general",
-      memberName: "guide",
-      description: "Get some guide links for the raid boss of your choice",
-      examples: [">guide cairn"],
-      args: [
-          {
-            key: "bossname",
-            prompt: "Which boss' guide are you looking for?",
-            type: "string"
-          }
-        ]
-    });
-  }
-
-  run(message, { bossname }) {
+module.exports = {
+  name: "guide",
+  description: "Get some guide links for the raid boss of your choice",
+  args: true,
+  execute(message, args) {
     try {
-      if (bossname === "cairn" || bossname === "cairncer") {
+      if (args[0] === "cairn" || args[0] === "cairncer") {
         return message.reply(`\n\nCairn the Indomitable - Raid 2 (Bastion of the Penitent), Boss 1
         \nWiki Guide: https://wiki.guildwars2.com/wiki/Bastion_of_the_Penitent#Cairn_the_Indomitable
         \nVideo Guide: https://www.youtube.com/watch?v=SjzG0qEi20o
         \nGood luck!`);
-      } else if (bossname === "mursaat overseer" || bossname === "mursaat" || bossname === "mo") {
+      } else if (args[0] === "mursaat overseer" || args[0] === "mursaat" || args[0] === "mo") {
         return message.reply(`\n\nMursaat Overseer - Raid 2 (Bastion of the Penitent), Boss 2
         \nWiki Guide: https://wiki.guildwars2.com/wiki/Bastion_of_the_Penitent#Mursaat_Overseer
         \nVideo Guide: https://www.youtube.com/watch?v=pcYEEX6RgDM
         \nGood luck!`);
-      } else if (bossname === "samarog" || bossname === "sam") {
+      } else if (args[0] === "samarog" || args[0] === "sam") {
         return message.reply(`\n\nSamarog - Raid 2 (Bastion of the Penitent), Boss 3
         \nWiki Guide: https://wiki.guildwars2.com/wiki/Bastion_of_the_Penitent#Samarog
         \nVideo Guide: https://www.youtube.com/watch?v=diwPG9Dsrt8
@@ -39,5 +23,5 @@ module.exports = class guidesCommand extends Command {
     } catch (err) {
       console.log(err);
     }
-  }
+  },
 };
