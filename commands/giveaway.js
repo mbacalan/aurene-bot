84f408e177b49bed9c0d2f9d2158414467106c43
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-01-31 15:44:07 +0300

    Initial giveaway create implementation

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const entries = JSON.parse(fs.readFileSync("./giveawayEntries.json", "utf8"));
+const entries = JSON.parse(fs.readFileSync("./giveawayData.json", "utf8"));
 
 module.exports = {
   name: "giveaway",
@@ -9,16 +9,28 @@ module.exports = {
   usage: "enter OR create",
   execute(message, args) {
     try {
-      const userId = entries[message.author.id];
-      // let activeGiveaway = currentGiveaway[guild.members];
-      if (args[0] === "enter") {
-        if (!userId) {
+      const entryCheck = entries[message.author.id];
+      const activeGiveaway = entries["currentGiveaway"];
+      if (args[0] === "create") {
+        if (!activeGiveaway) {
+          entries["currentGiveaway"] = {
+            userId: message.author.id,
+            username: message.author.username,
+            discriminator: message.author.discriminator,
+            creatingTime: `${message.createdAt}`,
+          };
+          fs.writeFile("./giveawayData.json", JSON.stringify(entries, null, "\t"));
+          return message.reply("you are creating a giveaway!");
+        }
+        return message.reply("please wait for the ongoing giveaway to end.");
+      } else if (args[0] === "enter") {
+        if (!entryCheck) {
           entries[message.author.id] = {
             username: message.author.username,
             discriminator: message.author.discriminator,
             entryTime: `${message.createdAt}`,
           };
-          fs.writeFile("./giveawayEntries.json", JSON.stringify(entries, null, "\t"));
+          fs.writeFile("./giveawayData.json", JSON.stringify(entries, null, "\t"));
           return message.reply(`You have entered the giveaway as ${message.author.username}#${message.author.discriminator}!`);
         }
         return message.reply("You've already entered this giveaway!");
