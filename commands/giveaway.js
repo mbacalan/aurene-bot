fa1d9c01284ecba82fff4072ce29b47a6b215522
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-01-31 18:24:26 +0300

    Implement collector for user input and improve if's

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -13,27 +13,46 @@ module.exports = {
       const activeGiveaway = entries["currentGiveaway"];
       if (args[0] === "create") {
         if (!activeGiveaway) {
-          entries["currentGiveaway"] = {
-            userId: message.author.id,
-            username: message.author.username,
-            discriminator: message.author.discriminator,
-            creatingTime: `${message.createdAt}`,
-          };
-          fs.writeFile("./giveawayData.json", JSON.stringify(entries, null, "\t"));
-          return message.reply("you are creating a giveaway!");
+          message.reply("what would you like to giveaway?");
+
+          const filter = m => m.content.startsWith(">");
+          const collector = message.channel.createMessageCollector(filter, { max: 1, time: 15000 });
+
+          collector.on("collect", m => {
+            console.log(`Collected ${m.content}`);
+          });
+
+          collector.on("end", collected => {
+            entries["currentGiveaway"] = {
+              userId: message.author.id,
+              username: message.author.username,
+              discriminator: message.author.discriminator,
+              creatingTime: `${message.createdAt}`,
+              item: collected.content,
+            };
+            fs.writeFile("./giveawayData.json", JSON.stringify(entries, null, "\t"));
+            return message.channel.send(`@everyone, ${message.author} is giving away ${collected.content}!
+            Use \`\`>giveaway enter\`\` to have a chance at grabbing it!`);
+          });
+        } else if (activeGiveaway) {
+          return message.reply("please wait for the ongoing giveaway to end.");
         }
-        return message.reply("please wait for the ongoing giveaway to end.");
       } else if (args[0] === "enter") {
-        if (!entryCheck) {
-          entries[message.author.id] = {
-            username: message.author.username,
-            discriminator: message.author.discriminator,
-            entryTime: `${message.createdAt}`,
-          };
-          fs.writeFile("./giveawayData.json", JSON.stringify(entries, null, "\t"));
-          return message.reply(`You have entered the giveaway as ${message.author.username}#${message.author.discriminator}!`);
+        if (activeGiveaway) {
+          if (!entryCheck) {
+            entries[message.author.id] = {
+              username: message.author.username,
+              discriminator: message.author.discriminator,
+              entryTime: `${message.createdAt}`,
+            };
+            fs.writeFile("./giveawayData.json", JSON.stringify(entries, null, "\t"));
+            return message.reply(`You have entered the giveaway as ${message.author.username}#${message.author.discriminator}!`);
+          } else if (entryCheck) {
+            return message.reply("You've already entered this giveaway!");
+          }
+        } else if (!activeGiveaway) {
+          return message.reply("there is no active giveaway to enter!");
         }
-        return message.reply("You've already entered this giveaway!");
       }
     } catch (error) {
       console.log(error);
