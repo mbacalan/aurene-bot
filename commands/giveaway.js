1c7102048ab4dd393764df2f5011bc5eea211259
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-02-09 21:14:26 +0300

    Add 2nd collector and initial timeout
    Initial timeout/random selection isn't funtional

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const { prefix } = require("../bot_config.json");
 const entries = JSON.parse(fs.readFileSync("./giveawayData.json", "utf8"));
 
 module.exports = {
@@ -15,22 +14,42 @@ module.exports = {
       if (args[0] === "create") {
         if (!activeGiveaway) {
           message.channel.send("What would you like to giveaway?");
+          const filter = m => m.author.id === message.author.id;
+          const itemCollector = message.channel.createMessageCollector(filter, { max: 1, time: 15000 });
 
-          const filter = m => m.content.startsWith(prefix);
-          const collector = message.channel.createMessageCollector(filter, { max: 1, time: 15000 });
+          itemCollector.on("end", collectedItem => {
+            message.channel.send("And how long will the giveaway run for?");
+            const durationCollector = message.channel.createMessageCollector(filter, { max: 1, time: 15000 });
 
-          collector.on("end", collected => {
-            entries["currentGiveaway"] = {
-              userId: message.author.id,
-              username: message.author.username,
-              discriminator: message.author.discriminator,
-              creatingTime: `${message.createdAt}`,
-              item: collected.first().content,
-            };
-            fs.writeFile("./giveawayData.json", JSON.stringify(entries, null, "\t"));
-            message.channel.send("Alright, one last question: how long will the giveaway run for?");
-            return message.channel.send(`@everyone, ${message.author} is giving away ${collected.first().content}! Use \`\`>giveaway enter\`\` to have a chance at grabbing it!`);
-          });
+            durationCollector.on("end", collectedDuration => {
+              entries["currentGiveaway"] = {
+                userId: message.author.id,
+                username: message.author.username,
+                discriminator: message.author.discriminator,
+                creatingTime: `${message.createdAt}`,
+                item: collectedItem.first().content,
+                duration: collectedDuration.first().content,
+              };
+              fs.writeFile("./giveawayData.json", JSON.stringify(entries, null, "\t"));
+              return message.channel.send(`@everyone, ${message.author} is giving away ${collectedItem.first().content}! Use \`\`>giveaway enter\`\` to have a chance at grabbing it! The giveaway will end in ${collectedDuration.first().content}`);
+            }); // durationCollector
+          }); // itemCollector
+          const giver = Object.entries(entries)[0];
+          const item = Object.entries(entries)[4];
+          const duration = Object.entries(entries)[5];
+          if (duration.includes("hour")) {
+            const parsedDuration = parseInt(duration) * 3600;
+            setTimeout(() => {
+              const winner = entries[Math.floor(Math.random() * entries.length)];
+              return message.channel.send(`<@${winner}>, you won ${item} from ${giver}`);
+            }, parsedDuration);
+          } else if (duration.includes("min")) {
+            const parsedDuration = parseInt(duration) * 60;
+            setTimeout(() => {
+              const winner = entries[Math.floor(Math.random() * entries.length)];
+              return message.channel.send(`<@${winner}>, you won ${item} from <@${giver}>`);
+            }, parsedDuration);
+          }
         } else if (activeGiveaway) {
           return message.reply("please wait for the ongoing giveaway to end.");
         }
