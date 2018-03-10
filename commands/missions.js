4de862b5fd16376f568f6cffe2dcf9ff53dc36b8
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-03-10 11:53:55 +0300

    Make the start arguement only usable by owner, leaders and officers

--- commands/missions.js
+++ commands/missions.js
@@ -1,4 +1,4 @@
-const { prefix } = require("../bot_config.json");
+const { prefix, owner, leaders, officers } = require("../bot_config.json");
 
 module.exports = {
   name: "missions",
@@ -8,22 +8,24 @@ module.exports = {
   execute(message, args) {
     try {
       if (args[0] === "start") {
-        if (!args[1]) {
-          message.channel.send(`@everyone grab your omnomberry bars and apple ciders, it's time for Guild Missions! Come to our Guild Hall, join the squad and the voice channel, if you can.
-          \nHere are the links to help you with Trek and Bounty missions:
-          https://wiki.guildwars2.com/wiki/Guild_Trek#Possible_locations
-          https://wiki.guildwars2.com/wiki/Guild_Bounty#Possible_targets
-          If you need further help, you can use \`\`${prefix}mission trek\`\` and \`\`${prefix}missions bounty\`\` commands for more info.
-          \nLet's go get those <:commendation:326054375317307402>'s!`);
-        } else if (args[1]) {
-          message.channel.send(`@everyone ready your omnomberry bars and apple ciders, Guild Missions are starting in **${args[1]}**! We'll meet at our Guild Hall, create a squad and move on from there.`);
-        }
+        if (message.author.id === owner || message.member.roles.has(leaders) || message.member.roles.has(officers)) {
+          if (!args[1]) {
+            return message.channel.send(`@everyone grab your omnomberry bars and apple ciders, it's time for Guild Missions! Come to our Guild Hall, join the squad and the voice channel, if you can.
+            \nHere are the links to help you with Trek and Bounty missions:
+            https://wiki.guildwars2.com/wiki/Guild_Trek#Possible_locations
+            https://wiki.guildwars2.com/wiki/Guild_Bounty#Possible_targets
+            If you need further help, you can use \`\`${prefix}mission trek\`\` and \`\`${prefix}missions bounty\`\` commands for more info.
+            \nLet's go get those <:commendation:326054375317307402>'s!`);
+          } else if (args[1]) {
+            message.channel.send(`@everyone ready your omnomberry bars and apple ciders, Guild Missions are starting in **${args[1]}**! We'll meet at our Guild Hall, create a squad and move on from there.`);
+          }
+        } return message.reply("only the owner, leaders and officers can use this command.");
       } else if (args[0] === "bounty" || args[0] === "Bounty") {
-        message.channel.send(`\nGuild Bounties are open world guild missions where members work together to kill targets across Tyria within 10-20 minutes. These NPCs are randomly chosen from a short list.
+        return message.channel.send(`\nGuild Bounties are open world guild missions where members work together to kill targets across Tyria within 10-20 minutes. These NPCs are randomly chosen from a short list.
         \nPlease check https://wiki.guildwars2.com/wiki/Guild_Bounty#Possible_targets to see how/where you can find the chosen target and bring justice to them.
         \nIf you have guild mates with you, wait for them to come along before you attack the target. Good luck!`);
       } else if (args[0] === "trek" || args[0] === "Trek") {
-        message.channel.send(`\nGuild Treks are open world guild missions where members work together to find several locations across Tyria within a set time limit. These locations are randomly chosen from a list of 180 possible locations.
+        return message.channel.send(`\nGuild Treks are open world guild missions where members work together to find several locations across Tyria within a set time limit. These locations are randomly chosen from a list of 180 possible locations.
         \nPlease check https://wiki.guildwars2.com/wiki/Guild_Trek#Possible_locations to see how/where you can find the chosen trek.
         \nIf you're not doing a trek alone, wait for everyone else to come before you interact with it. Good luck!`);
       }
