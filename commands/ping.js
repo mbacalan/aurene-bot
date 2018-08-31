edb044f03341358c7fc2db23dca48e9f0c90119e
Muhammed Bacalan <muhammedbacalan@gmail.com>
2018-08-31 11:43:14 +0300

    refactor: Use an embed for ping
    Instead of sending a plain message, try to send a better looking embed.
    If the bot doesn't have permissions to send an embed, it will inform the
    message author of it and send a plain message instead.

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,21 @@
+const { RichEmbed } = require("discord.js");
+
 module.exports = {
   name: "ping",
   aliases: ["pong"],
   description: "Get the avarage heartbeat ping of the websocket",
   execute(message) {
     try {
-      message.channel.send(`${message.client.ping}ms`);
+      const pingEmbed = new RichEmbed()
+        .setColor("#1a9306")
+        .setTitle("Pong 🏓")
+        .addField("Websocket Latency:", `${message.client.ping}ms`, true);
+
+      message.channel.send(pingEmbed)
+        .catch(() => {
+          message.reply("it looks like I don't have permissions to send an embed. Here is a boring version instead:");
+          message.channel.send(`Websocket latency is ${message.client.ping}ms.`);
+        });
     } catch (err) {
       console.log(err);
     }
