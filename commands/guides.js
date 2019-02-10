16e9d09d8608a074a1906cba9ff7effe88aeed02
Muhammed Bacalan <muhammedbacalan@gmail.com>
2019-02-10 16:19:58 +0300

    Fix wrong character for process.env

--- commands/guides.js
+++ commands/guides.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/local", ({
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/local", ({
   useNewUrlParser: true,
 }));
 
