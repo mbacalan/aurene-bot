09673ba0632f92abda543c4a632ed9aa6b517ca9
Muhammed Bacalan <muhammedbacalan@gmail.com>
2019-05-10 23:28:36 +0300

    Simplfy guides code
    Instead of hardcoded values to check against, we use a dynamic boss var
    for possible boss names wih a fallback to boss aliases. Since code is
    executed each time command is called from Discord, boss' value
    is always re-evaluated properly.

--- commands/guides.js
+++ commands/guides.js
@@ -7,44 +7,21 @@ module.exports = {
   usage: "bossname",
   async execute(message, args) {
 
-    async function bossInfo() {
-      let boss = await Bosses.findOne({ name: args.join(" ") });
-      if (!boss) {
-        boss = await Bosses.findOne({ alias: args[0] });
-      }
-      const bossName = boss.name.charAt(0).toUpperCase() + boss.name.slice(1);
-      return message.channel.send(`${bossName} - ${boss.raidIndex}
-      \nWiki Guide: ${boss.wiki}
-      \nVideo Guide: ${boss.video}
-      \nGood luck!`);
+    let boss = await Bosses.findOne({ name: args.join(" ") });
+    if (!boss) {
+      boss = await Bosses.findOne({ alias: args[0] });
     }
 
     try {
-      if (args[0] === "cairn" || args[0] === "cairncer") {
-        return bossInfo();
-      } else if (args[0] === "mursaat" && args[1] === "overseer" || args[0] === "mo") {
-        return bossInfo();
-      } else if (args[0] === "samarog" || args[0] === "sam") {
-        return bossInfo();
-      } else if (args[0] === "vale" && args[1] === "guardian" || args[0] === "vg") {
-        return bossInfo();
-      } else if (args[0] === "gorseval" || args[0] === "gorse") {
-        return bossInfo();
-      } else if (args[0] === "sabetha" || args[0] === "sab") {
-        return bossInfo();
-      } else if (args[0] === "slothasor" || args[0] === "sloth") {
-        return bossInfo();
-      } else if (args[0] === "bandit" && args[1] === "trio" || args[0] === "trio") {
-        return bossInfo();
-      } else if (args[0] === "matthias" || args[0] === "matt") {
-        return bossInfo();
-      } else if (args[0] === "escort" && args[1] === "glenna" || args[0] === "escort") {
-        return bossInfo();
-      } else if (args[0] === "keep" && args[1] === "construct" || args[0] === "kc") {
-        return bossInfo();
-      } else if (args[0] === "xera" || args[0] === "xera") {
-        return bossInfo();
-      } return message.reply("I couldn't find info about that boss");
+      if (boss) {
+        const bossName = boss.name.charAt(0).toUpperCase() + boss.name.slice(1);
+        return message.channel.send(`${bossName} - ${boss.raidIndex}
+      \nWiki Guide: ${boss.wiki}
+      \nVideo Guide: ${boss.video}
+      \nGood luck!`);
+      } else {
+        message.reply("I couldn't find info about that boss");
+      }
     } catch (err) {
       console.log(err);
     }
