22a64b6e5702d50b63ca0ca845030ecec2deddfd
Muhammed Bacalan <muhammedbacalan@gmail.com>
2019-05-10 18:33:41 +0300

    Round the latency number for better UX

--- commands/ping.js
+++ commands/ping.js
@@ -9,12 +9,12 @@ module.exports = {
       const pingEmbed = new RichEmbed()
         .setColor("#1a9306")
         .setTitle("Pong 🏓")
-        .addField("Websocket Latency:", `${message.client.ping}ms`, true);
+        .addField("Websocket Latency:", `${Math.round(message.client.ping)}ms`, true);
 
       message.channel.send(pingEmbed)
         .catch(() => {
           message.reply("it looks like I don't have permissions to send an embed. Here is a boring version instead:");
-          message.channel.send(`Websocket latency is ${message.client.ping}ms.`);
+          message.channel.send(`Websocket latency is ${Math.round(message.client.ping)}ms.`);
         });
     } catch (err) {
       console.log(err);
