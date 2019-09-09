3573780339331d77d8d74ba577e978a767f2175b
Muhammed Bacalan <muhammedbacalan@gmail.com>
2019-09-09 20:29:31 +0300

    Handle ongoing giveaway when bot restarts
    When bot is ready, check if there is a giveaway and re-set the timeout.
    This way, if bot loses connection to Discord before timeout is over,
    a new timeout will be created.
    
    Fix #5

--- bot.js
+++ bot.js
@@ -2,6 +2,40 @@ const fs = require("fs");
 const discord = require("discord.js");
 const commandFiles = fs.readdirSync("./commands");
 const mongoose = require("mongoose");
+const moment = require("moment");
+const { Giveaway, Entries, Winner } = require("./dbModels/models");
+
+function createWinner(winner, item) {
+  Winner.create({
+    userId: winner.userId,
+    userName: winner.userName,
+    discriminator: winner.discriminator,
+    item: item,
+  });
+}
+
+async function pickWinner() {
+  const winner = await Entries.aggregate([{ $sample: { size: 1 } }]);
+  return winner[0];
+}
+
+function clearGiveawayAndEntries() {
+  Giveaway.collection.deleteMany({});
+  Entries.collection.deleteMany({});
+}
+
+async function endGiveaway(giveaway, channel, item) {
+  const winner = await pickWinner();
+  if (!winner) {
+    channel.send("Looks like no one entered the giveaway :(");
+    console.error(`No one entered the giveaway of ${item}.`);
+    return clearGiveawayAndEntries();
+  }
+  createWinner(winner, item);
+  console.log(`The giveaway for ${item} ended, ${winner.userName}#${winner.discriminator} won.`);
+  channel.send(`Congratulations <@${winner.userId}>, you won **${item}** from ${giveaway.userName}#${giveaway.discriminator}!`);
+  clearGiveawayAndEntries();
+}
 
 // Get an instance of Discord Client
 const bot = new discord.Client();
@@ -23,13 +57,22 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log("Succesfully connected to database"));
 
 // Do these when bot is ready
-bot.on("ready", () => {
+bot.on("ready", async () => {
+  bot.user.setActivity("Guild Wars 2");
   console.log(`Logged in as ${bot.user.username}#${bot.user.discriminator} (ID:${bot.user.id})`);
   console.log(`Invite link is: https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=1`);
-  bot.user.setActivity("Guild Wars 2");
   console.log(`Bot's presence is set to: ${bot.user.presence.game.name}`);
   console.log(`Bot is in: ${bot.guilds.size} servers`);
   console.log("Awaiting orders...");
+
+  const giveawayChannel = bot.channels.get(process.env.GIVEAWAY_CHANNEL);
+  const giveaway = await Giveaway.find({});
+
+  if (giveaway[0]) {
+    const item = giveaway[0].item;
+    const timeout = giveaway[0].endTime - moment();
+    setTimeout(() => endGiveaway(giveaway[0], giveawayChannel, item), timeout);
+  }
 });
 
 bot.on("message", message => {
