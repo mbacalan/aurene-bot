04df49c9629124a8dddcffea3328f4274c96cfe7
Muhammed Bacalan <muhammedbacalan@gmail.com>
2020-01-22 21:07:34 +0300

    Add rebuildcache command
    For forcing a re-cache, owner only

---
+++ commands/rebuildCache.js
@@ -0,0 +1,24 @@
+const { buildDbFromApi } = require("../utils/caching");
+const logger = require("../utils/logger");
+
+class RebuildCache {
+  constructor() {
+    this.name = "rebuildcache";
+    this.description = "Rebuild Cache";
+  }
+
+  async execute(message) {
+    if (message.author.id !== process.env.OWNER) {
+      return;
+    }
+
+    logger.info("(Re)building API cache");
+    await message.client.user.setStatus("dnd");
+    await message.client.user.setActivity("Building API Cache", { type: "LISTENING" });
+    await buildDbFromApi();
+    await message.client.user.setStatus("online");
+    await message.client.user.setActivity("Guild Wars 2");
+  }
+}
+
+module.exports = new RebuildCache();
