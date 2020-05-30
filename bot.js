9946488372446a11c44f1926a145f8f3056bb13e
Muhammed Bacalan <muhammedbacalan@gmail.com>
2020-05-30 12:49:58 +0300

    Add starboard functionality
    Messages starred will be sent to a predefined channel on the server.

--- bot.js
+++ bot.js
@@ -39,6 +39,18 @@ bot.on("message", async message => {
   await executeCommand(bot, message);
 });
 
+bot.on("messageReactionAdd", async (reaction, author) => {
+  const starboard = bot.commands.get("starboard");
+
+  await starboard.addedReaction(bot, reaction, author);
+});
+
+bot.on("messageReactionRemove", async (reaction, author) => {
+  const starboard = bot.commands.get("starboard");
+
+  await starboard.removedReaction(bot, reaction, author);
+});
+
 bot.on("error", error => logger.error("General error:", error));
 process.on("unhandledRejection", error => logger.error("Uncaught Promise Rejection:", error));
 
---
+++ commands/starboard.js
@@ -0,0 +1,82 @@
+const { MessageEmbed } = require("discord.js");
+
+// TODO: DRY
+class Starboard {
+  constructor() {
+    this.name = "starboard";
+  }
+
+  async addedReaction(bot, reaction, author) {
+    const starChannel = bot.channels.cache.get(process.env.STARBOARD_CHANNEL);
+    const message = reaction.message;
+
+    if (reaction.emoji.name !== "⭐" || message.author.id === author.id || message.author.bot || !starChannel) return;
+
+    const fetchedMessages = await starChannel.messages.fetch({ limit: 100 });
+    const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith("⭐") && m.embeds[0].footer.text.endsWith(message.id));
+
+    // Edit the embed if the message is already on the starboard
+    if (stars) {
+      const star = /^⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
+      const starEmbed = await starChannel.messages.fetch(stars.id);
+      const foundStarEmbed = stars.embeds[0];
+      const foundStarMessage = foundStarEmbed.fields.find(field => field.name === "Message");
+
+      const embed = new MessageEmbed()
+        .addField("Author", message.author, true)
+        .addField("Channel", message.channel, true)
+        .addField("Message", foundStarMessage.value)
+        .setTimestamp()
+        .setFooter(`⭐ ${parseInt(star[1]) + 1} | ${message.id}`);
+
+      await starEmbed.edit({ embed });
+    }
+
+    // Send a new embed if the message not on the starboard
+    if (!stars) {
+      const starReactions = message.reactions.cache.get("⭐");
+
+      if (starReactions.count < 3) return;
+
+      const embed = new MessageEmbed()
+        .addField("Author", message.author, true)
+        .addField("Channel", message.channel, true)
+        .addField("Message", message.cleanContent)
+        .setTimestamp(new Date())
+        .setFooter(`⭐ 1 | ${message.id}`);
+
+      await starChannel.send({ embed });
+    }
+  }
+
+  async removedReaction(bot, reaction, author) {
+    const starChannel = bot.channels.cache.get(process.env.STARBOARD_CHANNEL);
+    const message = reaction.message;
+
+    if (reaction.emoji.name !== "⭐" || message.author.id === author.id || message.author.bot || !starChannel) return;
+
+    const fetchedMessages = await starChannel.messages.fetch({ limit: 100 });
+    const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith("⭐") && m.embeds[0].footer.text.endsWith(message.id));
+
+    // Reduce the amount of stars if the message is already on starboard, remove if no stars left
+    if (stars) {
+      const star = /^⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
+      const starEmbed = await starChannel.messages.fetch(stars.id);
+      const foundStarEmbed = stars.embeds[0];
+      const foundStarMessage = foundStarEmbed.fields.find(field => field.name === "Message");
+
+      if (parseInt(star[1]) - 1 == 0) return starEmbed.delete({ timeout: 1000 });
+
+      const embed = new MessageEmbed()
+        .addField("Author", message.author, true)
+        .addField("Channel", message.channel, true)
+        .addField("Message", foundStarMessage.value)
+        .setTimestamp()
+        .setFooter(`⭐ ${parseInt(star[1]) - 1} | ${message.id}`);
+
+      await starEmbed.edit({ embed });
+    }
+  }
+}
+
+module.exports = new Starboard;
