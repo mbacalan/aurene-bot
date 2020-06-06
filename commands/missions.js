2ae2fcb1a966899198490d1643ee7def665ccaaf
Muhammed Bacalan <muhammedbacalan@gmail.com>
2020-06-06 20:32:05 +0300

    Refine missions

--- commands/missions.js
+++ commands/missions.js
@@ -3,38 +3,30 @@ const logger = require("../utils/logger");
 class Missions {
   constructor() {
     this.name = "missions";
-    this.aliases = ["gm", "guildmissions", "mission"];
-    this.args = true;
-    this.description = "Alert members for starting Guild Missions and provide useful info to them";
+    this.aliases = ["gm", "guildmissions"];
+    this.description = "Alert members for joining Guild Missions and provide useful info to them";
   }
 
-  // TODO: What the heck is this
-  execute(message, args, isOwner, isRanking) {
+  async execute(message, isOwner, isRanking) {
     try {
-      if (args[0] === "start") {
-        if (isOwner || isRanking) {
-          if (!args[1]) {
-            return message.channel.send(`@everyone grab your omnomberry bars and apple ciders, it's time for Guild Missions! Come to our Guild Hall, join the squad and the voice channel, if you can.
-            \nHere are the links to help you with Trek and Bounty missions:
-            https://wiki.guildwars2.com/wiki/Guild_Trek#Possible_locations
-            https://wiki.guildwars2.com/wiki/Guild_Bounty#Possible_targets
-            If you need further help, you can use \`\`${process.env.PREFIX}mission trek\`\` and \`\`${process.env.PREFIX}missions bounty\`\` commands for more info.
-            \nLet's go get those <:commendation:326054375317307402>'s!`);
-          } else if (args[1]) {
-            return message.channel.send(`@everyone ready your omnomberry bars and apple ciders, Guild Missions are starting in **${args[1]}**! We'll meet at our Guild Hall, create a squad and move on from there.`);
-          }
-        } return message.reply("only the owner, leaders and officers can use this command.");
-      } else if (args[0] === "bounty" || args[0] === "Bounty") {
-        return message.channel.send(`\nGuild Bounties are open world guild missions where members work together to kill targets across Tyria within 10-20 minutes. These NPCs are randomly chosen from a short list.
-        \nPlease check https://wiki.guildwars2.com/wiki/Guild_Bounty#Possible_targets to see how/where you can find the chosen target and bring justice to them.
-        \nIf you have guild mates with you, wait for them to come along before you attack the target. Good luck!`);
-      } else if (args[0] === "trek" || args[0] === "Trek") {
-        return message.channel.send(`\nGuild Treks are open world guild missions where members work together to find several locations across Tyria within a set time limit. These locations are randomly chosen from a list of 180 possible locations.
-        \nPlease check https://wiki.guildwars2.com/wiki/Guild_Trek#Possible_locations to see how/where you can find the chosen trek.
-        \nIf you're not doing a trek alone, wait for everyone else to come before you interact with it. Good luck!`);
+      // TODO: Investigate negative checks for when both isOwner and isRanking is needed
+      if (isOwner || isRanking) {
+        const missionsChannel = message.client.channels.cache.get(process.env.MISSIONS_CHANNEL);
+        const membersToMention = missionsChannel.members.array().map((member) => `<@!${member.id}>`);
+
+        return message.channel.send(
+          `${membersToMention.join(", ")}, grab your omnomberry bars and apple ciders, it's time for Guild Missions!` +
+          "\nCome to our Guild Hall to join the squad." +
+          "\n\nHere are the links to help you with Trek and Bounty missions:" +
+          "\n<https://wiki.guildwars2.com/wiki/Guild_Trek#Possible_locations>" +
+          "\n<https://wiki.guildwars2.com/wiki/Guild_Bounty#Possible_targets>" +
+          "\n\nLet's go get those <:commend:528271266864431134>'s!"
+        );
       }
+
+      return message.reply("only the owner, leaders and officers can use this command.");
     } catch (error) {
-      logger.error("Error in missions command", error);
+      logger.error("Error in missions command: ", error);
     }
   }
 }
