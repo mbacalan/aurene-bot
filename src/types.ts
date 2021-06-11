698c07c5db8a4433e87761c608439ef1feead3eb
Muhammed Bacalan <muhammedbacalan@gmail.com>
2021-06-11 13:55:24 +0300

    Add interfaces for mongoose models

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import { Message } from "discord.js";
+import { Moment } from "moment";
 
 declare module "discord.js" {
   interface Client {
@@ -33,3 +34,89 @@ export interface CommandParams {
   isOwner: boolean,
   isRanking: boolean,
 };
+
+// Mongoose Models
+export interface IConfig {
+  prefix: string,
+  leaders?: number,
+  officers?: number,
+  giveawayChannel?: number,
+  missionsChannel?: number,
+  starboardChannel?: number,
+  giveawayRole?: number
+}
+
+export interface IUser {
+  userId: string,
+  userTag: string,
+}
+
+export interface IAchievement {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export interface IGiveaway {
+  _id: string,
+  userId: string,
+  userTag: string,
+  creationTime: Date | Moment,
+  endTime: Date | Moment,
+  item: string,
+  duration: string,
+  entries: IUser[]
+}
+
+export interface IGuild {
+  _id: string,
+  config: IConfig,
+  giveaways: IGiveaway[],
+  giveawayWinners: [],
+}
+
+export interface IBoss {
+  name: string,
+  alias: string,
+  wiki: string,
+  video: string,
+  raidIndex: string
+}
+
+export interface IBuild {
+  build: string
+}
+
+export interface IKey {
+  discordId: string,
+  keyName: string,
+  accountName: string,
+  permissions: string,
+  key: string
+}
+
+export interface ISpecialization {
+  id: number,
+  name: string,
+  profession: string,
+  elite: boolean,
+  icon: string,
+  professionIcon: string
+}
+
+export interface ITitle {
+  id: number,
+  name: string
+}
+
+export interface IWinner {
+  userId: string,
+  userTag: string,
+  item: string
+}
+
+export interface IWorld {
+  id: number,
+  name: string,
+  population?: string
+}
