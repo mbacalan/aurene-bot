0a076718b43a53bf7de532ffdbeb4fad716827c0
Muhammed Bacalan <muhammedbacalan@gmail.com>
2021-06-12 18:17:57 +0300

    Move gameData and emojiData to their own data folder

---
+++ src/data/index.ts
@@ -0,0 +1,10 @@
+import gameData from "./game";
+import { pollEmojiUnicodes, pollEmojis, roleEmojiUnicodes, roleEmojis } from "./emoji";
+
+export {
+  gameData,
+  pollEmojiUnicodes,
+  pollEmojis,
+  roleEmojiUnicodes,
+  roleEmojis
+}
